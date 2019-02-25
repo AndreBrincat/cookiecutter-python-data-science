acd6fc6daa4cae303903eebe9c77686f3f92daf6
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-02-24 17:27:30 -0800

    Minor formatting for repo name

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='{{ cookiecutter.python_module_name }}',
     packages=find_packages('src'),
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}'
 )
